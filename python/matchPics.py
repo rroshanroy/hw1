15e852f4c48ab9ec64e4dd08cd5aeeaeda1c9f76
Roshan Roy <rroshanroy@gmail.com>
2023-09-19 01:03:58 +0000

    First working version of code

--- python/matchPics.py
+++ python/matchPics.py
@@ -5,6 +5,8 @@ from helper import briefMatch
 from helper import computeBrief
 from helper import corner_detection
 
+from matplotlib import pyplot as plt
+
 # Q2.1.4
 
 def matchPics(I1, I2, opts):
@@ -27,15 +29,22 @@ def matchPics(I1, I2, opts):
         
 
         # TODO: Convert Images to GrayScale
+        img1 = cv2.cvtColor(I1, cv2.COLOR_BGR2GRAY)
+        img2 = cv2.cvtColor(I2, cv2.COLOR_BGR2GRAY)
         
         
         # TODO: Detect Features in Both Images
+        feats1 = corner_detection(img1, sigma)
+        feats2 = corner_detection(img2, sigma)
+        
         
         
         # TODO: Obtain descriptors for the computed feature locations
-        
+        desc1, locs1 = computeBrief(img1, feats1)
+        desc2, locs2 = computeBrief(img2, feats2)
 
         # TODO: Match features using the descriptors
-        
+        matches = briefMatch(desc1, desc2, ratio)
 
+        # plotMatches(img1, img2, matches, locs1, locs2)
         return matches, locs1, locs2
